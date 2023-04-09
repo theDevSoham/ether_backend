9cd9d5e0a07e8ebf88ccb11611720941bd8056e4
Soham Das <publicsoham.24@gmail.com>
2023-04-09 16:45:41 +0530

    transaction function added

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-const express = require('express');
-const { Network, Alchemy } = require('alchemy-sdk');
+const express = require("express");
+const { Network, Alchemy, Wallet, Utils } = require("alchemy-sdk");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 const port = 8080;
+const { PRIVATE_KEY, API_KEY } = process.env;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const settings = {
-    apiKey: "sqzilZlnbccL5yIcVN-k9rR1hF37QwNR",
-    network: Network.ARB_GOERLI,
+//Alchemy Settings
+
+const getAlchemy = () => {
+    const settings = {
+        apiKey: API_KEY,
+        network: Network.ETH_GOERLI,
+    };
+    return new Alchemy(settings);
 };
 
-app.get('/', async(req, res) => {
-    const alchemy = new Alchemy(settings);
+app.get("/", async(req, res) => {
+    const alchemy = getAlchemy();
     const latestBlock = await alchemy.core.getBlock("latest");
     res.json({ latestBlock: latestBlock });
+});
+
+app.post("/send", async(req, res) => {
+    const alchemy = getAlchemy();
+    const wallet = new Wallet(PRIVATE_KEY);
+    const { to, value } = req.body;
+
+    const transaction = {
+        to: "0x0B53E89cFD388f54A3683AAfc5974db4593B6641",
+        value: Utils.parseEther("0.001"),
+        gasLimit: "21000",
+        maxPriorityFeePerGas: Utils.parseUnits("5", "gwei"),
+        maxFeePerGas: Utils.parseUnits("20", "gwei"),
+        nonce: await alchemy.core.getTransactionCount(wallet.getAddress()),
+        type: 2,
+        chainId: 5, // Corresponds to ETH_GOERLI
+    };
+
+    try {
+        const rawTransaction = await wallet.signTransaction(transaction);
+        const response = await alchemy.transact.sendTransaction(rawTransaction);
+
+        res.status(200).json({ receipt: response });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({ error: error.message });
+    }
 });
 
 app.listen(port, () => {
